5358e7abe223180b5027f2b9a466b8eb14e8f85b
Alison Williams <apw5011@gmail.com>
2020-12-02 20:39:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,6 +12,7 @@ class Anagram
     # take the initialized word and splits it into array of single letters
     letter_array = @word.split("")
     # split the argument array into separate words
+    
     # make each word into its own array 
     # sort each array 
     # compare each of these arrays to the letter_array 
