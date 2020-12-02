19e1d92ea90fd380d6f692c77627b15d8c53bd76
Alison Williams <apw5011@gmail.com>
2020-12-02 20:36:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,5 @@
 # Your code goes here!
+require 'pry'
 class Anagram
   
   attr_accessor :word
@@ -8,8 +9,14 @@ class Anagram
   end 
   
   def match(array) 
-    
-    @word.split(" ")
+    # take the initialized word and splits it into array of single letters
+    letter_array = @word.split(" ")
+    binding.pry
+    # split the argument array into separate words
+    # make each word into its own array 
+    # sort each array 
+    # compare each of these arrays to the letter_array 
+    # figure out with word from the argument array to return 
     
     
     
