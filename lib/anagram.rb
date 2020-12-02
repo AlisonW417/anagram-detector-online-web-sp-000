5d15613dacd2d7757c50966354715c1d87240fec
Alison Williams <apw5011@gmail.com>
2020-12-02 21:13:50 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,23 +12,6 @@ class Anagram
     array.select do |w|
       w.split("").sort == @word.split("").sort 
     end 
-    # take the initialized word and splits it into array of single letters
-    letter_array = @word.split("")
-    # split the argument array into separate words
-    comparison_array = []
-    array.each { |w| comparison_array << w.split("")}
-    comparison_array.select do |x|
-      if x.sort == letter_array.sort
-        anagram = x.join
-      end
-    end 
-        
-    
-      
-    # make each word into its own array 
-    # sort each array 
-    # compare each of these arrays to the letter_array 
-    # figure out with word from the argument array to return 
     
     
     
